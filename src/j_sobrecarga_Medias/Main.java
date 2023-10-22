d371d811ca9afd3710bff1f23a5dfa6ef2d6d7c5
Alejandro Alonso <aalonsopuig@gmail.com>
2023-10-22 16:10:39 +0200

    update

--- src/j_sobrecarga_Medias/Main.java
+++ src/j_sobrecarga_Medias/Main.java
@@ -1,7 +1,18 @@
 package j_sobrecarga_Medias;
 
+/**
+ * Programa que resuelve este ejercicio:
+ * Calcular la media de dos o tres números utilizando sobrecarga de métodos.
+ *  
+ * @author Alejandro Alonso Puig
+ * @version 1.0 (Octubre 2023)
+ */
 public class Main {
 
+	/**
+	 * Main es el método al que se llama por defecto
+	 * @param args argumentos de entrada, que en este caso no se usan
+	 */
 	public static void main(String[] args) {
 			
 		Media Media1 = new Media(4.2, 2.5);
--- src/j_sobrecarga_Medias/Media.java
+++ src/j_sobrecarga_Medias/Media.java
@@ -1,17 +1,35 @@
 package j_sobrecarga_Medias;
 
+/**
+ * Clase que contiene los métodos que calculan la media
+ */
 public class Media {
 
 	private double media = 0;
 	
+	/**
+	 * Constructor para cálculo de media
+	 * @param a	Un valor de entrada
+	 * @param b Otro valor de entrada
+	 */
 	public Media (double a, double b){
 		media = (a+b)/2;
 	}
-	
+
+	/**
+	 * Constructor sobrecargado para cálculo de media de tres números
+	 * @param a Primer número
+	 * @param b Segundo número
+	 * @param c Tercer número
+	 */
 	public Media (double a, double b, double c){
 		media =(a+b+c)/3;
 	}
 
+	/**
+	 * Getter de resultado
+	 * @return Resultado de la media realizada por el constructor
+	 */
 	public String getMedia () {
 		return ("Media: " + media);
 	}
